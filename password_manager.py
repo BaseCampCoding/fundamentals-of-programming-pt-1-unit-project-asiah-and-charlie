98396d49e795a4c4f99596df1a26cfbc36c4bec3
Charles-Mardis <72397256+Charles-Mardis@users.noreply.github.com>
2020-10-05 16:09:09 -0500

    Made the program print the input from the user

--- password_manager.py
+++ password_manager.py
@@ -5,4 +5,12 @@ application_1 = input("What application are you using? ")
 username_1 = input(f"What's your username for {application_1}? ")
 email_1 = input(f"What's your email for {application_1}? ")
 password_1 = input(f"What's your password for {application_1}? ")
+cat = input(str("Which application do you need information for? ")).lower()
+list_1 = []
+list_1.append(username_1)
+list_1.append(email_1)
+list_1.append(password_1)
 
+if cat == application_1:
+    for i in list_1:
+      print(i)
