66ad2125c14fc27901f9756b5eefec4474430d07
Charles-Mardis <72397256+Charles-Mardis@users.noreply.github.com>
2020-10-07 11:33:29 -0500

    Added a new file, and added a for loop in it.

---
+++ computing.py
@@ -0,0 +1,5 @@
+def printing_info(info: str, account: str)-> float:
+    for inf, x in account:
+        if inf == account:
+            pass
+    return accounts
--- password_manager.py
+++ password_manager.py
@@ -1,41 +1,23 @@
-<<<<<<< HEAD
-=======
-#this program will prompt the user for which application they would like to remember their passwords for. 
-#it will also return the passowrds, emails, and usernames they enter for their applications
->>>>>>> 005e6297cadd0887b5084a6dea63092a108058c3
-accounts = [
-  ['facebook', 'Charles', 'person@whatever', '1234'],
-  ['instagram', 'Asiah', 'person@whatever', '1243'],
-]
+import computing
+accounts = []
 print("Welcome! ")
 while True:
   answer = input("Which would you like to do? [save, change, view] ")
   if answer == 'save':
-    application_1 = input("What application are you using? ")
-    username_1 = input(f"What's your username for {application_1}? ")
-    email_1 = input(f"What's your email for {application_1}? ")
-    password_1 = input(f"What's your password for {application_1}? ")
-  elif answer == 'change':
-    cat = input(str("Which application do you need information for? "))
-    username_1 = input(f"What's your username for {application_1}? ")
-    email_1 = input(f"What's your email for {application_1}? ")
-    password_1 = input(f"What's your password for {application_1}? ")
-  elif answer == 'view':
-    cat = input("Which application do you need information for? ")
-    if cat == application_1:
-      for i in accounts:
-        print(accounts[0])
+    for a in answer:
+      application_1 = input("What application are you using? ")
+      username_1 = input(f"What's your username for {application_1}? ")
+      email_1 = input(f"What's your email for {application_1}? ")
+      password_1 = input(f"What's your password for {application_1}? ")
+      accounts.append({'name': application_1, 'username': username_1, 'email': email_1, 'password': password_1})
       break
-<<<<<<< HEAD
-    elif cat == application_2:
-=======
-    elif cat == application_1:
->>>>>>> 005e6297cadd0887b5084a6dea63092a108058c3
-      for i in accounts:
-        print(accounts[1])
-        break
-    else: 
-      print("Sorry, that application isn't saved. Try again. ")
+  elif answer == 'change':
+    pass  
+  elif answer == 'view':
+    info = input("What account are you needing information for? ")
+    if computing.printing_info(info, accounts):
+      print("There you go! ")
+
   else:
     print("Invalid option, try again! ")
   
