19332e5113629ffefa85b5436b83a57691bbab77
AsiahM <72397300+AsiahM@users.noreply.github.com>
2020-10-07 15:34:25 -0500

    FIX SAVE

--- password_manager.py
+++ password_manager.py
@@ -4,15 +4,14 @@ import computing
 accounts = []
 print("Welcome! ")
 while True:
+  print(accounts)
   answer = input("Which would you like to do? [save, change, view] ")
   if answer == 'save':
-    for a in answer:
       application_1 = input("What application are you using? ")
       username_1 = input(f"What's your username for {application_1}? ")
       email_1 = input(f"What's your email for {application_1}? ")
       password_1 = input(f"What's your password for {application_1}? ")
       accounts.append({'account': application_1, 'username': username_1, 'email': email_1, 'password': password_1})
-      break
   elif answer == 'change':
     pass  
   elif answer == 'view':
